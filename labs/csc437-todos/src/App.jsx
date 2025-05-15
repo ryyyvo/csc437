bd9647369b0edbbfd81cba2f31e3b13fa803f68a
ryyyvo <38389871+ryyyvo@users.noreply.github.com>
2025-05-15 00:08:25 -0700

    Finish lab 13 portion

--- labs/csc437-todos/src/App.jsx
+++ labs/csc437-todos/src/App.jsx
@@ -1,24 +1,21 @@
 import './App.css'
 import TodoItem from './components/TodoItem'
+import AddTaskForm from './components/AddTaskForm';
 
 
 function App(props) {
     const taskList = props.tasks?.map((task) => (
-        <TodoItem id={task.id} name={task.name} completed={task.completed} />
+        <TodoItem 
+            key={task.id}
+            id={task.id} 
+            name={task.name} 
+            completed={task.completed} 
+        />
     ));
 
     return (
         <main className="m-4"> 
-            <div className="flex mb-4">
-                <input 
-                  placeholder="New task name"
-                  className="border border-gray-300 rounded px-3 py-2 mr-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-medium px-4 py-2 rounded transition-colors cursor-pointer">
-                  Add task
-                </button>
-            </div>
-
+            <AddTaskForm />
             <section>
                 <h1 className="text-xl font-bold">To do</h1>
                 <ul>
---
+++ labs/csc437-todos/src/components/AddTaskForm.jsx
@@ -0,0 +1,15 @@
+function AddTaskForm() {
+    return (
+        <div className="flex mb-4">
+            <input
+                placeholder="New task name"
+                className="border border-gray-300 rounded px-3 py-2 mr-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <button className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-medium px-4 py-2 rounded transition-colors cursor-pointer">
+                Add task
+            </button>
+        </div>
+    );
+}
+
+export default AddTaskForm;
