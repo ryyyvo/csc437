2aae3213c953e798c0a51c0255a2830158e44faa
ryyyvo <38389871+ryyyvo@users.noreply.github.com>
2025-05-28 19:06:45 -0700

    Implement waitDuration to delay fetch

--- labs/lab18/packages/backend/src/index.ts
+++ labs/lab18/packages/backend/src/index.ts
@@ -9,13 +9,18 @@ const STATIC_DIR = process.env.STATIC_DIR || "public";
 
 const app = express();
 
+function waitDuration(numMs: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, numMs));
+}
+
 app.use(express.static(STATIC_DIR));
 
 app.get("/api/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
 
-app.get("/api/images", (req: Request, res: Response) => {
+app.get("/api/images", async (req: Request, res: Response) => {
+    await waitDuration(1000); // Wait 1 second
     const images = fetchDataFromServer();
     res.json(images);
 });
