e44639bd3f2c0e124cd334872d779176f95a2d10
ryyyvo <38389871+ryyyvo@users.noreply.github.com>
2025-05-29 21:05:37 -0700

    Add initial ImageProvider.ts

---
+++ labs/lab18/packages/backend/src/ImageProvider.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from "mongodb";
+
+interface IImageDocument {
+    // TODO
+}
+
+export class ImageProvider {
+    private collection: Collection<IImageDocument>
+
+    constructor(private readonly mongoClient: MongoClient) {
+        const collectionName = process.env.IMAGES_COLLECTION_NAME;
+        if (!collectionName) {
+            throw new Error("Missing IMAGES_COLLECTION_NAME from environment variables");
+        }
+        this.collection = this.mongoClient.db().collection(collectionName);
+    }
+
+    getAllImages() {
+        return this.collection.find().toArray(); // Without any options, will by default get all documents in the collection as an array.
+    }
+}
